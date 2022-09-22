d79ee8cd5c402bc40e2e87a9afc17b884b5930b5
Neha <nehashaikh470@gmail.com>
2022-09-22 18:25:04 +0200

    pokemon images added

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -10,7 +10,7 @@ const Pokemon = (poke) => {
    console.log(pokedata);
     return (
    <>
-    <h1>PokemonList</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" style={{ width:"200px" }}/>
 
     <div className="pokemon-container">
       {pokedata.map((pk) => {
@@ -18,9 +18,9 @@ const Pokemon = (poke) => {
                         <li key={pk.id}>
                           
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pk.id}.png`}  />
       <Card.Body>
-        <Card.Title>{pk.name.english}</Card.Title>
+        <h4>{pk.name.english}</h4>
         <Link to={`/pokemon/${pk.id}`}>View Details</Link>
       </Card.Body>
     </Card>
